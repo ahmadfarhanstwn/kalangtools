d46bba776fae16b40b4d952016719b7af4d6718f
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-03-11 02:07:18 +0700

    server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,18 @@
-const OrganizationIdPage = () => {
+import { db } from "../../../../../lib/db";
+import { Board } from "./_components/board";
+import { Form } from "./_components/form";
+
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany()
+
     return ( 
-        <div>
-            Organization Page
+        <div className="flex flex-col space-y-4">
+            <Form />
+            <div className="space-y-2">
+                {boards.map((board) => (
+                    <Board key={board.id} id={board.id} title={board.title} />
+                ))}
+            </div>
         </div>
      );
 }
